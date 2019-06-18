3a95ed06a0916c4cfea8fafadf916abfbb1710ea
Karam Kabbara <contact@karam.io>
2019-06-18 22:58:29 +0100

    Now caching assets.

--- sw.js
+++ sw.js
@@ -1,11 +1,30 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons"
+];
+
 // install service worker
 self.addEventListener("install", evt => {
-  console.log("Service Worker has been installed!");
+  //console.log("Service Worker has been installed!");
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log("Caching shell assets.");
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener("activate", evt => {
-  console.log("Service Worker has been activated!");
+  //console.log("Service Worker has been activated!");
 });
 
 // fetch event (required for install banner)
