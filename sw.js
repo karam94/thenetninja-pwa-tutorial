2530df69e802f114126bee0361c00d9a37262ff9
Karam Kabbara <contact@karam.io>
2019-06-19 21:19:45 +0100

    Added dynamic cache.

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ const dynamicCacheName = "site-dynamic-v1";
 const assets = [
   "/",
   "/index.html",
-  "/pages/fallback.html",
   "/js/app.js",
   "/js/ui.js",
   "/js/materialize.min.js",
@@ -11,15 +10,27 @@ const assets = [
   "/css/materialize.min.css",
   "/img/dish.png",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
-  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+  "/pages/fallback.html"
 ];
 
-// install service worker
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+// install event
 self.addEventListener("install", evt => {
-  //console.log("Service Worker has been installed!");
+  //console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then(cache => {
-      console.log("Caching shell assets.");
+      console.log("caching shell assets");
       cache.addAll(assets);
     })
   );
@@ -27,31 +38,41 @@ self.addEventListener("install", evt => {
 
 // activate event
 self.addEventListener("activate", evt => {
-  //console.log("Service Worker has been activated!");
+  //console.log('service worker activated');
   evt.waitUntil(
     caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key)));
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+          .map(key => caches.delete(key))
+      );
     })
   );
 });
 
-// fetch event (required for install banner)
+// fetch event
 self.addEventListener("fetch", evt => {
-  //console.log("Fetch event.", evt);
+  //console.log('fetch event', evt);
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          return fetchRes;
-        })
-      });
-    }).catch(() => {
-      if(evt.request.url.indexOf(".html") > -1){
-        return caches.match("/pages/fallback.html");
-      }
-    })
+    caches
+      .match(evt.request)
+      .then(cacheRes => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              limitCacheSize(dynamicCacheName, 15);
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/pages/fallback.html");
+        }
+      })
   );
 });
